175cf4ed5a7552ed08229f9c5f13930f05e52b22
Ali <aliga@code.edu.az>
2022-11-04 19:50:03 +0400

    initial commit

---
+++ demoappweb/demoappweb/Program.cs
@@ -0,0 +1,15 @@
+namespace demoappweb
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            var app = builder.Build();
+
+            
+
+            app.Run();
+        }
+    }
+}
